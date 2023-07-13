72aecf023083402a022f2e8adb6e4c34c09b51b9
samiunique <yitesam@gmail.com>
2023-07-13 16:10:31 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the string
+ * "Programming is like building a multilingual puzzle"
+ * enclosed in double quotation marks.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) {
     puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
