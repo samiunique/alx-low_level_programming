a21987e2c6004659a42179974f4c19c928a2690b
samiunique <yitesam@gmail.com>
2023-07-13 15:32:23 -0700

    funtion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
+int main(void)
+{
+    puts(""Programming is like building a multilingual puzzle");
     return 0;
 }
 
