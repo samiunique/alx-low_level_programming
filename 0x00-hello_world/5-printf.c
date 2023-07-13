2ce72314f1c337ef7c211d762666d31a7bcadc4f
samiunique <yitesam@gmail.com>
2023-07-13 16:46:23 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function serves as the entry point of the program.
+ * It prints the string "Programming is like building a multilingual puzzle"
+ * enclosed in double quotation marks using the `puts` function.
+ * The main function returns an integer value of 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,");
+    return 0;
+}
