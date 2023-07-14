622ccb5b8f7cef5f78d8bfd36181fa53d37b12d5
samiunique <yitesam@gmail.com>
2023-07-13 17:12:52 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -12,8 +12,8 @@
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,");
-    printf("\n");
-    return (0);
+         printf("with proper grammar, but the outcome is a piece of art,");
+         printf("\n");
+         return (0);
 }
 
