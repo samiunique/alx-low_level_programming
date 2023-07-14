fbdbfa93b3c117b82c983c3624ce692d7c45ef05
samiunique <yitesam@gmail.com>
2023-07-13 19:34:28 -0700

    ouote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the string
+ * "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line character using the `puts` function.
+ * The main function returns an integer value of 0 to indicate successful exec.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        write("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+        write"\n");
+        return (0);
+}
