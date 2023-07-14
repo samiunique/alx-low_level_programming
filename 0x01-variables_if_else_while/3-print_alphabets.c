1d1352a12f551f81a81edfc751c4a5a61baa1d75
samiunique <yitesam@gmail.com>
2023-07-14 11:23:17 -0700

    low & upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the sequence of lowercase letters from
+ *              'a' to 'z', followed by the sequence of uppercase letters
+ *              from 'A' to 'Z', and finally the dollar sign ('$').
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('$');
+	
+	return (0);
+}
