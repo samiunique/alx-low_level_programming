f4a5b7471dcf56d385624986456dbd9c085457dd
samiunique <yitesam@gmail.com>
2023-07-14 08:02:12 -0700

    positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /* more headers goes there */
 /* add random no to variable n */
 /* betty style doc for function main goes there */
-/* read random no from variable n ade identify if is negative posetive or zero */
+/* main -read random no from variable n ade identify if is */
 int main(void)
 {
 	int n;
