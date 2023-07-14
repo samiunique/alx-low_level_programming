20c0197d2258fccf3b7cbeabe211d09153a0b001
samiunique <yitesam@gmail.com>
2023-07-14 10:30:49 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,40 +2,36 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there */
-/* add random no to variable n */
-/* betty style doc for function main goes there */
-/* main -read random no from variable n ade identify if is */
 /**
  * main - Entry point of the program
  *
- * Description: This function prints the string
- * "Programming is like building a multilingual puzzle"
- * enclosed in double quotation marks.
+ * Description: This function generates a random number and determines the
+ *              last digit of the number.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	int num;
+    int n;
+    int num;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-	num = n % 10;
-	/* your code goes there */
-        if (num > 5)
-        {
-                printf("Last digit of %d is %d and is greater than 5\n", n, num);
-        }
-        else if (num == 0)
-        {
-                printf("Last digit of %d is %d and is 0\n", n, num);
-        }
-        else
-        {
-                printf("Last digit of %d is %d and is less than 6 and not 0\n", n, num);
-        }
-	return (0);
-                                                                                                                                                                                             
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    num = n % 10;
+
+    /* your code goes here */
+    if (num > 5)
+    {
+        printf("The last digit of %d is %d and is greater than 5\n", n, num);
+    }
+    else if (num == 0)
+    {
+        printf("The last digit of %d is %d and is 0\n", n, num);
+    }
+    else
+    {
+        printf("The last digit of %d is %d and is less than 6\n", n, num);
+    }
+
+    return (0);
 }
