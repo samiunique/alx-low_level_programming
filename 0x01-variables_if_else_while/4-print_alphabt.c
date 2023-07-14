8de5ab8b5419e64b896c86458074c3c98ea716b3
samiunique <yitesam@gmail.com>
2023-07-14 11:45:47 -0700

    eliminate

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function generates a random number and determines the
+ *              last digit of the number.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+	if (ch != 'e' && ch != 'q')
+	{
+                putchar(ch);
+        }
+	}
+
+        putchar('\n');
+
+        return (0);
+}
