64f641a55eaa60dea57828b86af1884bf1472b75
samiunique <yitesam@gmail.com>
2023-07-14 11:08:49 -0700

    low case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function generates a random number and determines the
+ *              last digit of the number.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+{
+	puchar(ch);
+}
+
+
+	return (0);
+}
