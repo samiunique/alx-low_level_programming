cd7ec3c27bb3726e01d21aaf9c84b7d5e35c7ee5
samiunique <yitesam@gmail.com>
2023-07-13 17:10:23 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  * Description: This function prints the string
  * "with proper grammar, but the outcome is a piece of art,"
  * followed by a new line character using the `puts` function.
- * The main function returns an integer value of 0 to indicate successful execution.
+ * The main function returns an integer value of 0 to indicate successful exec.
  *
  * Return: Always 0 (Success)
  */
