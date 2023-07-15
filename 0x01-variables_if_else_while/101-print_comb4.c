017c7bba96a3fb6077cc918b360b8a37b2538351
samiunique <yitesam@gmail.com>
2023-07-14 20:18:55 -0700

    separet and combine

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,31 +2,36 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Description: This function generates a random number and determines the
- *              last digit of the number.
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
 	int i, j, k;
+	int count = 0;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 1; j <= 6; j++)
-		{
-			for (k = 2; k <= 6; k++)
-			{
-				printf("%d%d%d", i, j, k);
+	for (i = 0; i <= 9; i++) {
+		for (j = 1; j <= 6; j++) {
+			for (k = 2; k <= 6; k++) {
+				if (count < 599) {
+					printf("%d%d%d", i, j, k);
+					count += 3;
 
-				if (!(i == 9 && j == 6 && k == 6))
-				{
-					printf(", ");
+					if (!(i == 9 && j == 6 && k == 6)) {
+						if (count < 599) {
+							printf(", ");
+							count += 2;
+						} else {
+							break;
+						}
+					}
+				} else {
+					break;
 				}
 			}
+			if (count >= 599) {
+				break;
+			}
+		}
+		if (count >= 599) {
+			break;
 		}
 	}
 
