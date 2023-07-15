ea076bf03831a13bc9a5b23c28ae179e5aa1369e
samiunique <yitesam@gmail.com>
2023-07-14 19:50:07 -0700

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function generates a random number and determines the
+ *              last digit of the number.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
