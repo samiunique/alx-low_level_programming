9897320892ae37581bd475fd56d2b9216c5852c9
samiunique <yitesam@gmail.com>
2023-07-14 19:30:12 -0700

    base10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -12,14 +12,15 @@
  */
 int main(void)
 {
-        char ch;
+	char ch;
 
-        for (ch = '0'; ch <= '9'; ch++)
-        {
-                putchar(ch);
-        }
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
+
