002ef36d3b897a3269a40e60f81599b43e147524
samiunique <yitesam@gmail.com>
2023-07-16 01:04:32 -0700

    constant

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@
  */
 int main(void)
 {
-	char* a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	*char a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i = 0;
 	
 	while (a[i] != '\0')
