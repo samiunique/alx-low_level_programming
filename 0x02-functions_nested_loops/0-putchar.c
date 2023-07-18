6e595ace27a25e096b30493df4e6a674005ab831
samiunique <yitesam@gmail.com>
2023-07-18 07:29:52 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the string
+ * "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line character using the `puts` function.
+ * The main function returns an integer value of 0 to indicate successful exec.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *a = "_putchar";
+	int i = 1;
+
+	while (a[i] != '\0')
+	{
+	putchar(a[i]);
+	i++;
+	}
+
+	putchar('\n');
+	return (1);
+}
