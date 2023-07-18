0c1a25aa044058f591991252c86ceaac77a53c9a
samiunique <yitesam@gmail.com>
2023-07-18 15:24:14 -0700

    check if its alpha or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - check for alpbets in any case
+ *
+ * @c: int argument
+ *
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+        return isalpha(c);
+}
