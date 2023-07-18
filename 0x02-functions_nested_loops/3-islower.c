a6cac57af17d5d45939935447a7a41f4baf72921
samiunique <yitesam@gmail.com>
2023-07-18 14:55:29 -0700

    check case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - function to print alphabet 10x
+ * _islower - check for lower case
  *
  * Description: This function generates alphabets in lowercase
  *
- * Return: void
+ * Return: 0
  */
 int _islower(int c)
 {
-	return c >= 'a' && c <= 'z';
+	return (c >= 'a' && c <= 'z');
 }
