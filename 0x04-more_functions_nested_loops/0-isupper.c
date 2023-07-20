792d9136df2b03820b31d6ca2cdb2dafa622a082
samiunique <yitesam@gmail.com>
2023-07-20 10:53:31 -0700

    check upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper case
+ *
+ * @c: int argument
+ *
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+more functions 
+0x04. C - More functions, more nested loops
