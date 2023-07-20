f4110f98f438004f1c57c11dd253597f7c8f6c82
samiunique <yitesam@gmail.com>
2023-07-20 14:54:36 -0700

    exept 2&4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,25 +2,23 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print numbers exept 2&4
+ * print_most_numbers - print numbers except 2 & 4
  *
  * Description: This function generates numbers
  *
  * Return: void
  */
 void print_most_numbers(void)
-        /* Function implementation */
 {
-        int ch;
+    int ch;
 
-        for (ch = 0; ch <= 9; ch++)
+    for (ch = 0; ch <= 9; ch++)
+    {
+        if (ch != 2 && ch != 4)
         {
-		if (ch != 2 && ch != 4)
-		{
-               		_putchar(ch);
-        	}
-	}
+            _putchar('0' + ch);
+        }
+    }
 
-        _putchar('\n');
-
+    _putchar('\n');
 }
