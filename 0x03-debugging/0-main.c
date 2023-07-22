ef58f3f1df9d749907f1eedca69561c5e93839d6
samiunique <yitesam@gmail.com>
2023-07-21 21:43:04 -0700

    positive or not

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * @i: int argument
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+
+    i = 0;
+
+    positive_or_negative(i);
+    /*positive_or_negative(0);
+    positive_or_negative(-1);
+    */
+    return (0);
+}
