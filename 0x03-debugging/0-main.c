79a7b5d2028c909301d5cd433e7a6e07eccf41fe
samiunique <yitesam@gmail.com>
2023-07-21 23:55:41 -0700

    copy

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,7 +3,7 @@
 /**
  * main - check the code.
  *
- * @i: int argument
+ * @i: int argument.
  * Return: Always 0.
  */
 int main(void)
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+/* add random no to variable n */
+/* betty style doc for function main goes there */
+/* main -read random no from variable n ade identify if is */
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the string
+ * "Programming is like building a multilingual puzzle"
+ * enclosed in double quotation marks.
+ *
+ * Return: Always 0 (Success)
+ */
+int postitive_or_negative(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
