aca87846aa135be54ca549e557c755ec843692f5
samiunique <yitesam@gmail.com>
2023-07-24 14:56:39 -0700

    using standard library strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strlen - gives length of string.
+ *
+ * @s: string parameter
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return strlen(s);
+}
