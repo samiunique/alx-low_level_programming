c80482912fb85405fcfd996fe070eb860b1303af
samiunique <yitesam@gmail.com>
2023-07-24 16:20:58 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: variable poited for character
+ *sami
+ * Return: Nothing
+*/
+
+void print_rev(char *s)
+{
+	int x;
+	
+	for (x = 0; s[x] != '\0'; ++x);
+
+
+	for (--x; x >= 0; --x)
+		_putchar(s[x]);
+	_putchar('\n');
+}
