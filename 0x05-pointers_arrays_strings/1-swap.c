a7f4cd92649114fa3f0cdb479022d0eb51ba5f6b
samiunique <yitesam@gmail.com>
2023-07-24 14:08:39 -0700

    exchenge value of variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+/**
+ * swap_int - exchenge vaue of two intigers using tempo var x  and *.
+ *
+ * @a: variable pointer for integer.
+ * @b: variable2 pointer for integer.
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a
+	*a = *b
+	*b = *x
+}
