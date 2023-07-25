ea1e73a52f8217648d1c01c4eb58c15cd6755604
samiunique <yitesam@gmail.com>
2023-07-24 18:43:03 -0700

    new line forbiden

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,11 +16,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d ", a[i]);
+		printf("%d", a[i]);
 			if (a[i] != 98)
 			{
-				 printf(",");
+				 printf(", ");
 			}
 	}
-	/*printf("\n");*/
+	printf("\n");
 }
