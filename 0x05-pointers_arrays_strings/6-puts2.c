710aae1b0c2ca00a9fac677ff4bc5c3cb613a181
samiunique <yitesam@gmail.com>
2023-07-24 17:11:12 -0700

    even

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+/**
+ * puts2 - print even no.
+ *
+ * @str: string points for int.
+ *
+ * Return: even
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 1, 3, 5, 9)
+			putchar(str[i]);
+	}
+	putchar("\n");
+}
