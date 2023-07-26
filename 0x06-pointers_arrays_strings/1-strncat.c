8677986489bb2961dc017cc9c8cc57d072327caf
samiunique <yitesam@gmail.com>
2023-07-26 16:41:06 -0700

    faul on betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,30 +2,29 @@
 #include <string.h>
 
 /**
- * _strcat - function to concatenate 2 strings.
+ * _strncat - function to concatenate 2 strings with n.
  *
  * @dest: pointer to file destination.
  * @src: pointer to file source.
+ * @n: it will use at most n bytes from
  *
  * Return: pointer to resulting string @dest.
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = strlen(dest);
+	int dest_len = strlen(dest);
     int src_len = strlen(src);
     int copy_len = (n < src_len) ? n : src_len;
 	int i;
     /*/ Copy the first 'copy_len' characters from 'src' to 'dest'.*/
-    for (i = 0; i < copy_len; i++) {
-        dest[dest_len + i] = src[i];
-    }
-
-    /*/ Null-terminate the concatenated string in 'dest'.*/
-    dest[dest_len + copy_len] = '\0';
-
-    /* Use the standard library strncat to concatenate the remaining characters.*/
-    strncat(dest, src + copy_len, n - copy_len);
-
-    return (dest);
+	for (i = 0; i < copy_len; i++) 
+	{
+		dest[dest_len + i] = src[i];
+	}
+	/*/ Null-terminate the concatenated string in 'dest'.*/
+	dest[dest_len + copy_len] = '\0';
+	/* Use the standard library strncat to concatenate the remaining characters.*/
+	strncat(dest, src + copy_len, n - copy_len);
+	return (dest);
 }
