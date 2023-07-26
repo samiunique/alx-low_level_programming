4f30ca94cdf1c76bde782a13e752dd4df974b8e0
samiunique <yitesam@gmail.com>
2023-07-26 15:34:30 -0700

    betty 😢

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,6 +3,8 @@
 
 
 /**
+ * _strcat - function that concatenates 2 strings
+ *
  * @dest: destination of string to move.
  * @src: source of strin that move.
  *
@@ -11,5 +13,5 @@
 
 char *_strcat(char *dest, char *src)
 {
-	 return strcat(dest, src);
+	return strcat(dest, src);
 }
