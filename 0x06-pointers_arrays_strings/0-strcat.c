0847b00e4b96783cf0d4312076bf6ce07227e4ad
samiunique <yitesam@gmail.com>
2023-07-26 13:37:16 -0700

    cocatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+/**
+ * @dest: destination of string to move.
+ * @src: source of strin that move.
+ *
+ * Retun: concatenates
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	concatenate(s1, s2);
+
+	return(0);
+}
