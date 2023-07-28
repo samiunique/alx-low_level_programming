9badba0dd45c89dd468d0478b446541c337a249d
samiunique <yitesam@gmail.com>
2023-07-27 19:22:03 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,23 +9,36 @@
 
 char *cap_string(char *s)
 {
-	int i;
-	bool capitalize_next = true;
+	int i = 0;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
-        {
-            str[i] = str[i] - 'a' + 'A';
-        }
-
-        capitalize_next = false;
-
-        if (is_separator(str[i]))
-        {
-            capitalize_next = true;
-        }
-    }
-
-    return str;
+	/*iterate through our array values*/
+	while (s[i] != '\0')
+	{
+		/*check for any lowercase letters*/
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			/**
+			 * if we have a null character
+			 * change its value to capital
+			*/
+			if (i == 0)
+			{
+				s[i] -= 32;
+			}
+			/**
+			 * if we find any character matching the below before any small
+			 * letter we change that value to a capital letter.
+			*/
+			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+				s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+				s[i - 1] == 124)
+			{
+				s[i] -= 32;
+			}
+		}
+		i++;
+	}
+	return (s);
 }
