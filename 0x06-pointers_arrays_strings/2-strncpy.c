66ff1ee5ca83bdec203aada6ab4b73005d4affbe
samiunique <yitesam@gmail.com>
2023-07-28 10:43:22 -0700

    ()

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,6 +6,7 @@
  *
  * Return: Pointer to the resulting string @dest.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
     int i;
@@ -21,5 +22,5 @@ char *_strncpy(char *dest, char *src, int n)
         dest[i] = '\0';
     }
 
-    return dest;
+    return (dest);
 }
