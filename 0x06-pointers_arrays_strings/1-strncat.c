c0dfe1fd1fbd6bf312afb1a8109bee94a71cf75c
samiunique <yitesam@gmail.com>
2023-07-27 17:53:48 -0700

    betty 🤦‍♂️

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,14 +18,14 @@ char *_strncat(char *dest, char *src, int n)
     int copy_len = (n < src_len) ? n : src_len;
     int i;
 
-    /*/ Copy the first 'copy_len' characters from 'src' to 'dest'.*/
+    /* Copy the first 'copy_len' characters from 'src' to 'dest'.*/
     for (i = 0; i < copy_len; i++)
     {
         dest[dest_len + i] = src[i];
     }
-    /*/ Null-terminate the concatenated string in 'dest'.*/
+    /* Null-terminate the concatenated string in 'dest'.*/
     dest[dest_len + copy_len] = '\0';
-    /*/ Use the standard library strncat to concatenate the remaining characters.*/
+    /* Use the standard library strncat to concatenate the remaining characters.*/
     strncat(dest, src + copy_len, n - copy_len);
 
     return (dest);
