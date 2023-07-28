a78a615f9048127b2cf8b54173911b04f8369921
samiunique <yitesam@gmail.com>
2023-07-28 11:20:12 -0700

    betty

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
-
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    {
+	int i;
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
 	}
 	for (; i < n; i++)
-    {
-        dest[i] = '\0';
-    }
-
-    return (dest);
+	{
+		dest[i] = '\0';
+	}
+	
+	return (dest);
 }
