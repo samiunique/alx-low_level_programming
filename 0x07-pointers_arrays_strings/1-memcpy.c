a6eb7d393b1096dd695d64b7ea7a96aa2f061028
samiunique <yitesam@gmail.com>
2023-07-31 07:41:21 -0700

    memory task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+/**
+ * _memcpy - function copies n bytes from memory area src to memory area dest
+ *	including the terminating null byte (\0)
+ *
+ * @src: source of string parameter input
+ * @dest: destination of string
+ *
+ * Return: pointer to dest input parameter
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int a, b = 0;
+
+	for (a = 0; src[a] != '\0'; ++a)
+	{
+		dest[b] = src[a];
+		++b;
+	}
+	dest[b] = '\0';
+
+	return (dest);
+}
