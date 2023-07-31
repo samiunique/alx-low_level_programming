8d7bf7c32f43b8e004c13dd6e073265ee6c91aed
samiunique <yitesam@gmail.com>
2023-07-31 15:53:50 -0700

    copy memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,6 +4,7 @@
  *
  * @src: source of string parameter input
  * @dest: destination of string
+ * @n: n byte of src
  *
  * Return: pointer to dest input parameter
 */
@@ -11,11 +12,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
-
+	
 	for (a = 0; a < n; ++a)
 	{
 		dest[a] = src[a];
 	}
-
+	
 	return (dest);
 }
