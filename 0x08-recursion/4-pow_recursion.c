b8f793111f204eb843d96a5496d0ec115df02bcf
samiunique <yitesam@gmail.com>
2023-08-02 16:56:21 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+/**
+ * _pow_recursion - a function that returns the power of @x by @y x^y
+ *
+ * 
+ * 
+ *
+ * @x: integer x
+ * @y: integer y
+ *
+ * Return: x the power of y
+*/
+int _pow_recursion(int x, int y)
+{
+	
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
