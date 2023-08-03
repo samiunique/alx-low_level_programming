fc9f1b968f07e50a8352bfc013302a3426e78d3f
samiunique <yitesam@gmail.com>
2023-08-02 17:38:51 -0700

    task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -14,7 +14,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
 		return (strlen(s));
+		_strlen_recursion(s + 1);
 	}
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,18 @@
+/**
+ * is_prime_number - a function that returns 0&10fpr prim or not
+ *
+ *
+ * @n: integer to check
+ * 
+ *
+ * Return: 0 or 1
+*/
+
+int is_prime_number(int n)
+{
+	n = -n;
+	if (n / 1 == n && n / n == 1)
+		return (1);
+	else
+		return (0);
+}
