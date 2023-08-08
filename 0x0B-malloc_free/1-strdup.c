97e61153d717510c095d16876a051ab8abea371c
samiunique <yitesam@gmail.com>
2023-08-08 04:44:47 -0700

    coreect mixed declaration coppy and dupplicate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,22 +2,23 @@
 #include <stdlib.h>
 
 /**
- * _strdup -  a function that returns a pointer to a newly allocated space in memory.
+ * _strdup -  function to returns pointer to a newly allocated space in memory.
  *
  * @str: character pointer
  *
- * Return: returns a pointer to a new string 
+ * Return: returns a pointer to a new string
  */
 
 char *_strdup(char *str)
 {
 	char *x;
+	size_t len;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	size_t len = strlen(str);
+	len = strlen(str);
 	x = (char *)malloc(len + 1);
 	if (x == NULL)
 	{
