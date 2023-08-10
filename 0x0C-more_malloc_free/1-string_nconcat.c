28c5c6cdf7102b0f3e36fa6bce169a52f3ddca70
samiunique <yitesam@gmail.com>
2023-08-10 15:39:35 -0700

    corecctin

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = len_s2;
 	}
-	concatenated = (char *) malloc((len_s1 + n + 1) * sizeof(char));
+	concatenated = (char *)malloc((len_s1 + n + 1) * sizeof(char));
 	if (concatenated == NULL)
 	{
 		return (NULL);
