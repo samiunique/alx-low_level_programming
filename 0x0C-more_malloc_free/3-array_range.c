8b1a2431f5682477d9293615fb4aee631acd479e
samiunique <yitesam@gmail.com>
2023-08-10 17:47:33 -0700

    sorted int array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - fun to creates an array of integers.
+ *
+ * @min: minimum int.
+ * @max: maximum int.
+ *
+ * Return: pointer to the newly created array.
+ */
+
+int *array_range(int min, int max)
+{
+	int total;
+	int *arr;
+	int i;
+
+	total = max - min + 1;
+	arr = (int *)malloc(total * sizeof(int));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < total; i++)
+		arr[i] = min + i;
+
+	return (arr);
+}
