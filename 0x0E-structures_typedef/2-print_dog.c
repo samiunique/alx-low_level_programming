c39a70b38163600767f598ec865a36f4a4594bb3
samiunique <yitesam@gmail.com>
2023-08-15 17:38:59 +0300

    add nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,11 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	if (d->age == 0)
+		d->age = (0.0);
+
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
