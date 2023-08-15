9d7b8fdf005368e8ad754c5b3fc7986ad7541dac
samiunique <yitesam@gmail.com>
2023-08-16 01:14:51 +0300

    new

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - new pointer for copy of dog profile
+ *
+ * @name: name copy for dog name
+ * @owner: copy of owner name
+ * @age: copy of dog age
+ *
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog = (dog_t *)malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+		
+	dog->name = name;
+	dog->owner = owner;
+	dog->age = age;
+	return (dog);
+}
