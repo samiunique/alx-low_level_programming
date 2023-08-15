adec100f83723119cd2acf20ca3d6f83714a8a77
samiunique <yitesam@gmail.com>
2023-08-15 17:44:49 +0300

    not else we need to print nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 	if (d->age == 0)
 		d->age = (0.0);
 
-	else
+	/*else*/
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
