887c12301a244b3f6b6196e08ed83a9b40cf34e1
samiunique <yitesam@gmail.com>
2023-08-15 16:14:22 +0300

    redo task1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 #include <stdlib.h>
 /**
  * struct dog - typedef for dog profile
@@ -11,27 +10,10 @@
  *
  * Description: typedef for dog profile
  */
-
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
-/**
- * my_dog - tpedef my dog
- *
- * @name: first member name of dog
- * @owner: second member dog owner
- * @age: therd member dog age
- *
- * Return: dog
- */
-
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
