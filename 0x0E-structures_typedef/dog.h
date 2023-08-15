418a2ff08f3ecfd37d835674b0f170b9ddd96aae
samiunique <yitesam@gmail.com>
2023-08-15 03:40:27 +0300

    push dog.h and readme

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+/**
+ * struct dog - typedef for dog profile
+ *
+ * @name: first member name of dog
+ * @owner: second member dog owner
+ * @age: therd member dog age
+ *
+ * Description: typedef for dog profile
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+struct dog *my_dog(char *name, char *owner, float age)
+{
+	struct dog *ddog;
+
+	ddog = malloc(sizeof(struct dog));
+	if (ddog == NULL)
+		return (NULL);
+	ddog->name = name;
+	ddog->owner = owner;
+	ddog->age = age;
+	return (ddog);
+}
