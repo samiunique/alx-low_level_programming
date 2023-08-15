10ec8ff20d461e7a34363b42bfcc4d29fbc47a06
samiunique <yitesam@gmail.com>
2023-08-15 17:20:57 +0300

    update prototype and task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - print dog profile.
+ *
+ * @d: new pointer for dog profile.
+ *
+ * Return: return nil
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
+
