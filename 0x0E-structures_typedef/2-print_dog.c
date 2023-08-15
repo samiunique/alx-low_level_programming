3236657d110dbd39a28bb0243b05d8041888de37
samiunique <yitesam@gmail.com>
2023-08-15 17:27:34 +0300

    update nil output

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		d->name = "(nil)";
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
