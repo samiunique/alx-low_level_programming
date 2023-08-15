83bb7837bbad59fa0baa72ba2913580f59d96e80
samiunique <yitesam@gmail.com>
2023-08-15 05:50:06 +0300

    rework

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * init_dog - dog re arenge profle
+ *
+ * @name: first m
+ * @owner: second m
+ * @age: third m
+ * @d: 4th
+ * Description: dog profile
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		return;
-	
-	d = malloc(sizeof(struct dog));
-	
-		
-
-		d->name = name;
-		d->owner = owner;
-		d->age = age;
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
 }
