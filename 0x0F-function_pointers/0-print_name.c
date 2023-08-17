ae07213204000e020e4f0c40433171fb9ff50cab
samiunique <yitesam@gmail.com>
2023-08-16 18:51:20 -0700

    task 0 and header pushed

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_name - fuction to print name 
+ *
+ * @name: name of the person
+ * @f: variable for function pointer
+ *
+ * Return: name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
