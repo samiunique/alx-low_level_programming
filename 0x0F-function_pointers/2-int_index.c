b8f28ce7221c1c76aecbae14b19074aa67e1cd09
samiunique <yitesam@gmail.com>
2023-08-16 21:05:31 -0700

    task2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * int_index - index of int
+ *
+ * @array: array
+ * @size: size of
+ * @cmp: pointer
+ *
+ * Return: index unles not int
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL && cmp == NULL)
+	{		
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i] != 0))
+		    return (i);
+		 
+	}
+	}
+
+	return (-1);
+}
