65f2e484f4730451ca62e08e943525aa01c4d007
samiunique <yitesam@gmail.com>
2023-08-25 18:07:21 -0700

    si

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+/**
+ * struct - stru
+ * @list: list 
+ */
 typedef struct list_s
 {
 	char *str;
@@ -7,6 +12,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 
 #endif 
