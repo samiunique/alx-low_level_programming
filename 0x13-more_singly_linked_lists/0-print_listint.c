02dd0e0793d62fb4dab6f60856bb4db5defd74b6
samiunique <yitesam@gmail.com>
2023-08-28 17:28:14 +0300

    betty documentation and indentation

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,14 @@
 #include "lists.h"
+/**
+ * print_listint - print all nodes
+ * @h: linked lists
+ *
+ * Return: num
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
