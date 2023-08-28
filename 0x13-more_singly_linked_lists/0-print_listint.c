fde8c0a438aa5a26e3ca126a8d784e081e6a03ae
samiunique <yitesam@gmail.com>
2023-08-28 15:39:01 +0300

    readme prototype and pirint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
+
