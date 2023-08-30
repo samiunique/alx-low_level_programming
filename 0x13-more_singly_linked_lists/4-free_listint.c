b00bde292a11551f4f29bd1990a65d401d6ee77d
samiunique <yitesam@gmail.com>
2023-08-30 04:59:10 +0300

    update hrader & free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,12 @@
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	/*iterate through each node in list*/
+	while ((current = head) != NULL)
+	{
+		/*set head as the next node*/
+		head = head->next;
+		free(current);
+	}
+}
