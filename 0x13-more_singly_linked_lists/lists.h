f4cac4e82ea77ff88085525f81f660879ad0b60e
samiunique <yitesam@gmail.com>
2023-08-30 05:42:41 +0300

    insert @ index

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,8 @@ typedef struct listint_s
 		    struct listint_s *next;
 } listint_t;
 
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int sum_listint(listint_t *head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int pop_listint(listint_t **head);
