cd1dfffd367d11a82f94c06e9ac56241743a6faa
samiunique <yitesam@gmail.com>
2023-08-31 13:59:00 -0700

    betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,22 +10,22 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int converted;
-        converted = 0;
-if (b == NULL)
- {
-return (0);
-}
-		else 
-        while (*b)
-        {
-if (!isdigit(*b))
- {
-        return (0);
-}
 
-                converted = (converted << 1) | (*b - '0');
-				b++;
+	converted = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	else
+		while (*b)
+		{
+			if (!isdigit(*b))
+			{
+				return (0);
+			}
+			converted = (converted << 1) | (*b - '0');
+			b++;
 		}
-
-        return (converted);
+	return (converted);
 }
