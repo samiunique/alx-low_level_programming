49780edafeb6d798decf01f4f2a9c6b60cd4f012
samiunique <yitesam@gmail.com>
2023-09-04 17:33:58 -0700

    size typo error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (fileSize = 0; text_content[fileSise];)
+		for (fileSize = 0; text_content[fileSize];)
 			fileSize++;
 	}
 
