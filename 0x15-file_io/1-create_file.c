dc916d6f289d9f68dd7386e340e078a605ebd80f
samiunique <yitesam@gmail.com>
2023-09-04 17:22:14 -0700

    creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			fileSize++;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0644);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
 	w = write(o, text_content, fileSize);
 
 	if (o == -1 || w == -1)
