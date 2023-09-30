0989e5c274b77923a890c36522885d7d4b94d7fa
samiunique <yitesam@gmail.com>
2023-09-29 19:26:42 -0700

    readme header and task 1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int.
+ * @b: pointer to binary strings
+ *
+ * Return: converted
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int converted;
+
+	converted = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	else
+		while (*b)
+			if (!isdigit(*b))
+			{
+				return (0);
+			}
+	converted = (converted << 1) | (*b - '0');
+	b++;
+}
+return (converted);
+}
