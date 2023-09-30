fd7a55db00595a25fa99a9dcc03b9491e60de8df
samiunique <yitesam@gmail.com>
2023-09-29 20:00:20 -0700

    redo

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * clear_bit - clear
+ *
+ * @n: pointer to
+ * @index: starting
+ *
+ * Return: 1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+        unsigned long int i;
+
+        if (index > (sizeof(unsigned long int) * 8 - 1))
+                return (-1);
+        i = ~(1 << index);
+        *n = *n & i;
+
+        return (1);
+}
