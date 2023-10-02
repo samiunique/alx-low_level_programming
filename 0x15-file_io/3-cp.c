ad88f690e2edc6b625cd48a9fb741c8357673fa4
samiunique <yitesam@gmail.com>
2023-10-02 14:02:59 -0700

    betty description, 40line handeled , indent

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,13 +9,13 @@
  */
 char *create_buffer(void)
 {
-    char *buffer = malloc(BUFFER_SIZE);
-    if (buffer == NULL)
-    {
-        perror("malloc");
-        exit(98);
-    }
-    return buffer;
+	char *buffer = malloc(BUFFER_SIZE);
+
+	if (buffer == NULL)
+	{
+		perror("malloc");
+		exit(98); }
+	return (buffer);
 }
 
 /**
@@ -24,59 +24,53 @@ char *create_buffer(void)
  */
 void close_file(int fd)
 {
-    if (close(fd) == -1)
-    {
-        perror("close");
-        exit(100);
-    }
+	if (close(fd) == -1)
+	{
+		perror("close");
+		exit(100); }
 }
-
+/**
+ * main - main fun
+ * @argc: arg count
+ * @argv: arg pass to array
+ * Return: res
+*/
 int main(int argc, char *argv[])
 {
-    int from, to, r, w;
-    char *buffer;
+	int from, to, r, w;
+	char *buffer;
 
-    if (argc != 3)
-    {
-        fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
-        exit(97);
-    }
+	if (argc != 3)
+	{
+		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97); }
+	buffer = create_buffer();
 
-    buffer = create_buffer();
-
-    from = open(argv[1], O_RDONLY);
-    if (from == -1)
-    {
-        perror("open");
-        free(buffer);
-        exit(98);
-    }
-
-    to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (to == -1)
-    {
-        perror("open");
-        free(buffer);
-        close_file(from);
-        exit(99);
-    }
-
-    while ((r = read(from, buffer, BUFFER_SIZE)) > 0)
-    {
-        w = write(to, buffer, r);
-        if (w == -1)
-        {
-            perror("write");
-            free(buffer);
-            close_file(from);
-            close_file(to);
-            exit(99);
-        }
-    }
-
-    free(buffer);
-    close_file(from);
-    close_file(to);
-
-    return 0;
+	from = open(argv[1], O_RDONLY);
+	if (from == -1)
+	{
+		perror("open");
+		free(buffer);
+		exit(98); }
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to == -1)
+	{
+		perror("open");
+		free(buffer);
+		close_file(from);
+		exit(99); }
+	while ((r = read(from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w = write(to, buffer, r);
+		if (w == -1)
+		{
+			perror("write");
+			free(buffer);
+			close_file(from);
+			close_file(to);
+			exit(99); }}
+	free(buffer);
+	close_file(from);
+	close_file(to);
+	return (0);
 }
